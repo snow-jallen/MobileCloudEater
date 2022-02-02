31d5d9f652fb00e600229029018765a2475159a6
Jonathan Allen <jallen@snow.edu>
2022-02-02 16:26:42 -0700

    compiles but test fails

--- Mobile/Mobile/ViewModels/AboutViewModel.cs
+++ Mobile/Mobile/ViewModels/AboutViewModel.cs
@@ -1,29 +1,30 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
-namespace Mobile.ViewModels
+namespace Mobile.ViewModels;
+
+public partial class AboutViewModel : BaseViewModel
 {
-    public partial class AboutViewModel : BaseViewModel
+    public AboutViewModel()
     {
-        public AboutViewModel()
-        {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
-        }
-
-        public ICommand OpenWebCommand { get; }
-
-
-        [ICommand]
-        public async Task GetWeather()
-        {
-
-        }
-
-        public ObservableCollection<object> WeatherResults { get; private set; } = new();
+        Title = "About";
+        OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
     }
+
+    public ICommand OpenWebCommand { get; }
+
+
+    [ICommand]
+    public async Task GetWeather()
+    {
+        var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync("http://localhost:5005/WeatherForecast");
+    }
+
+    public ObservableCollection<object> WeatherResults { get; private set; } = new();
 }
--- Tests/WeatherForecastTests.cs
+++ Tests/WeatherForecastTests.cs
@@ -4,21 +4,20 @@ using NUnit.Framework;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Tests
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+namespace Tests;
 
-        [Test]
-        public async Task BasicConnection()
-        {
-            var aboutViewModel = new AboutViewModel();
-            await aboutViewModel.GetWeatherCommand.ExecuteAsync(this);
-            aboutViewModel.WeatherResults.Any().Should().BeTrue();
-        }
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task BasicConnection()
+    {
+        var aboutViewModel = new AboutViewModel();
+        await aboutViewModel.GetWeatherCommand.ExecuteAsync(this);
+        aboutViewModel.WeatherResults.Any().Should().BeTrue();
     }
 }
