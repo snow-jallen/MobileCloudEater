9e9abf9aae4d8afbc6ddd6e107789714efc610f2
Jonathan Allen <jallen@snow.edu>
2022-02-04 14:48:32 -0700

    deserialize json coming back from api; display in view

--- Mobile/Mobile/ViewModels/AboutViewModel.cs
+++ Mobile/Mobile/ViewModels/AboutViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Essentials;
@@ -22,9 +25,24 @@ public partial class AboutViewModel : BaseViewModel
     [ICommand]
     public async Task GetWeather()
     {
+        WeatherResults.Clear();
         var httpClient = new HttpClient();
-        var response = await httpClient.GetAsync("http://localhost:5005/WeatherForecast");
+        foreach (var result in await httpClient.GetFromJsonAsync<IEnumerable<WeatherForecast>>("http://localhost:5005/WeatherForecast"))
+        {
+            WeatherResults.Add(result);
+        }
     }
 
     public ObservableCollection<object> WeatherResults { get; private set; } = new();
+}
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
 }
