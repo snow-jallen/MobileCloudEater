bb14a3cbd80c91af57e5fd47e4ad3769beed899e
Jonathan Allen <jallen@snow.edu>
2022-02-10 12:44:09 -0700

    testing app service deployments

---
+++ API/Controllers/WhoAmIController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WhoAmIController : ControllerBase
+    {
+
+        [HttpGet(Name = "JoinGame")]
+        public async Task<string> Get()
+        {
+            return Environment.MachineName;
+        }
+    }
+}
